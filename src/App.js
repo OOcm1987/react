9ed19475dd62b7cad441fbe1715024ff8d141e00
Oscar CM <emilianocosteno@gmail.com>
2022-04-08 12:08:57 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
 
+import TableList from './components/TableList';
+
 function App() {
+  let nombre = "Jhon";
+  let auth = true;
+  let estaciones = ['primavera','otoño','verano','invierno'];
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <label htmlFor='nombre'>Nombre</label>
+        <input type="text" id='nombre' />
+        <h1>{nombre}</h1>
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          { auth ? 'logueado':'quien es' }
         </p>
+
+        <p>
+          Edit <code>src/App.js</code> and save to reload. recargar
+        </p>
+
+        <ul>
+          {
+            estaciones.map( (el, index) =>(
+              <li key={index}>{el}</li>
+            ))
+          }
+        </ul>
+
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React 123
         </a>
+        <section>
+          <TableList msg="soy un componente 22222ssss" />
+        </section>
       </header>
     </div>
   );
---
+++ src/components/TableList.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+// class TableList extends Component{
+//     render(){
+//         return <h2>{this.props.msg}</h2>
+//     }
+// }
+
+// function TableList(props){
+//     return <h2>{props.msg}</h2>
+// }
+
+const TableList = props => {
+    return <h2>{props.msg}</h2>
+}
+
+export default TableList;
